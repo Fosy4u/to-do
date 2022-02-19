c7677ae5ecdda964fccc030e6532faadef88891d
foster <foster.ogwudu@yahoo.com>
2022-02-19 22:19:17 +0000

    capturd input data

--- src/feature/to-do/components/InputComponent.jsx
+++ src/feature/to-do/components/InputComponent.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./InputComponent.css";
 
 const InputComponent = () => {
+  const [item, setItem] = useState();
+  console.log(item);
+  //handle input Change
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
   return (
     <div className="container">
-      <input id="toDo" placeholder="Enter your To-do tasks" />
+      <input
+        id="toDo"
+        placeholder="Enter your To-do tasks"
+        onChange={(e) => handleChange(e)}
+      />
+      <button> Add </button>
+
     </div>
   );
 };
