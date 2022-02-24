c3a6a245901b6e0844e79d6fbb047ab84cd573c6
foster <foster.ogwudu@yahoo.com>
2022-02-24 23:06:33 +0000

    captured inputValue

--- src/feature/to-do/components/InputComponent.jsx
+++ src/feature/to-do/components/InputComponent.jsx
@@ -2,22 +2,27 @@ import { useState } from "react";
 import "./InputComponent.css";
 
 const InputComponent = () => {
-  const [item, setItem] = useState();
-  console.log(item);
+  const [inputValue, setInputValue] = useState();
+  const [items, setItems] = useState([]);
+
+  console.log("Items is = ", items);
   //handle input Change
   const handleChange = (e) => {
-    setItem(e.target.value);
+    setInputValue(e.target.value);
+  };
+  const handleAddClick = () => {
+    setItems((items) => [...items, inputValue]);
   };
 
   return (
     <div className="container">
       <input
         id="toDo"
+        type="search"
         placeholder="Enter your To-do tasks"
         onChange={(e) => handleChange(e)}
       />
-      <button> Add </button>
-
+      <button onClick={handleAddClick}> Add </button>
     </div>
   );
 };
